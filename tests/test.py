b027523bdd9aedd42f0b33ce55941ea4d3950650
Timothy Campbell <tcampbel22@gmail.com>
2025-03-05 15:57:56 +0200

    Modifed test file and added requiremtn.txt

--- tests/test.py
+++ tests/test.py
@@ -4,24 +4,29 @@ import unittest
 from my_sum import sum
 from fractions import Fraction
 
-class TestSum(unittest.TestCase):
-	def test_list_int(self):
-		data = [1, 2, 3]
-		result = sum(data)
-		self.assertEqual(result, 6)
-	def test_list_fraction(self):
-		data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
-		result = sum(data)
-		self.assertEqual(result, 1)
-	def test_bad_type(self):
-		data = [1, 2, 3]
-		with self.assertRaises(TypeError):
-			result = sum(data)
-	# def test_sum(self):
-	# 	self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
-	# def test_sum_tuple(self):
-	# 	self.assertEqual(sum((1, 2, 2)), 6, "Should be 6)")
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+    def test_bad_type(self):
+        data = "Hello"
+        with self.assertRaises(TypeError):
+            result = sum(data)
+
+# def test_sum(self):
+# 	self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+# def test_sum_tuple(self):
+# 	self.assertEqual(sum((1, 2, 2)), 6, "Should be 6)")
+
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
